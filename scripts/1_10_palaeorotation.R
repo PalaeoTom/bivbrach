1069b4fbbd47e27600f26b30cd8550a79a0d8e0f
PalaeoTom <fo18751@bristol.ac.uk>
2025-05-26 19:10:40 +0100

    tweaked script - only using models that provide coordinates for all occurrences

--- scripts/1_10_palaeorotation.R
+++ scripts/1_10_palaeorotation.R
@@ -43,11 +43,11 @@ Peabody$midpoint <- as.numeric(Peabody$midpoint)
 
 #### Rotating for palaeocoordinates ####
 ## Rotate
-PBDB_rotated <- palaeorotate(PBDB, lng = "lng", lat = "lat", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA", "TorsvikCocks2017"), method = "point", uncertainty = T)
-GBIF_rotated <- palaeorotate(GBIF, lng = "decimalLongitude", lat = "decimalLatitude", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA", "TorsvikCocks2017"), method = "point", uncertainty = T)
-AMNH_rotated <- palaeorotate(AMNH, lng = "longitudeDecimal", lat = "latitudeDecimal", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA", "TorsvikCocks2017"), method = "point", uncertainty = T)
-NMS_rotated <- palaeorotate(NMS, lng = "longitude", lat = "latitude", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA", "TorsvikCocks2017"), method = "point", uncertainty = T)
-Peabody_rotated <- palaeorotate(Peabody, lng = "longitude", lat = "latitude", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA", "TorsvikCocks2017"), method = "point", uncertainty = T)
+PBDB_rotated <- palaeorotate(PBDB, lng = "lng", lat = "lat", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA"), method = "point", uncertainty = T)
+GBIF_rotated <- palaeorotate(GBIF, lng = "decimalLongitude", lat = "decimalLatitude", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA"), method = "point", uncertainty = T)
+AMNH_rotated <- palaeorotate(AMNH, lng = "longitudeDecimal", lat = "latitudeDecimal", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA"), method = "point", uncertainty = T)
+NMS_rotated <- palaeorotate(NMS, lng = "longitude", lat = "latitude", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA"), method = "point", uncertainty = T)
+Peabody_rotated <- palaeorotate(Peabody, lng = "longitude", lat = "latitude", age = "midpoint", model = c("MERDITH2021", "PALEOMAP", "GOLONKA"), method = "point", uncertainty = T)
 
 ## Save raw output
 saveRDS(PBDB, "data/PBDB/PBDB_1_10_1.Rds")
@@ -55,3 +55,5 @@ saveRDS(GBIF, "data/GBIF/GBIF_1_10_1.Rds")
 saveRDS(AMNH, "data/museum/AMNH_1_10_1.Rds")
 saveRDS(NMS, "data/museum/NMS_1_10_1.Rds")
 saveRDS(Peabody, "data/museum/Peabody_1_10_1.Rds")
+
+#### Get average coordinates for main analysis
