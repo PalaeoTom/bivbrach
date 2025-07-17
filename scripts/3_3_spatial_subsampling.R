a3093a5f9084eed9008ad81567d310c57bc3c440
PalaeoTom <fo18751@bristol.ac.uk>
2025-07-17 10:21:31 +0100

    updated spatial subsampling script to contain other form of standardisation

--- scripts/3_3_spatial_subsampling.R
+++ scripts/3_3_spatial_subsampling.R
@@ -28,6 +28,17 @@ home <- getwd()
 ## Set number of cores
 core.set <- 8
 
+## Output directory
+output.dir <- "~/Library/CloudStorage/Dropbox/unfinished_projects/bivalve_brachiopod/traybake_output"
+
+## Read in richness function
+source("functions/richness.R")
+
+## Set radius, nBatch and nBites
+radius <- 100000
+nBatch <- 500
+nBites <- 500
+
 #### Let's bake! ####
 ## Using 50km grid cells and 100km radius RCR as this yields greatest number of distinct clusters. Also retains over 64% of grid cells (5957) and over 71% of occurrences (111880).
 ## Maximum number of 50km grid cells in a 100km-radius RCR is 25.
@@ -42,13 +53,10 @@ genera$cellReef <- as.numeric(genera$cellReef)
 ## Read in genera count
 genera_count <- read.csv("data/sensitivity_testing/genera_50_radius_nSite_sensitivity_testing.csv", header = T, row.names = 1)
 
-## Isolate relevant counts
+## Set number of cells
 nCells <- 2
-radius <- 100000
 
-## Update number of batches to
-nBatch <- 500
-nBites <- 500
+## Identify relevant data from results of sensivity analysis
 rel <- genera_count[intersect(which(genera_count$grid_cells == nCells), which(genera_count$radius == radius)),]
 
 ## Inititlise cluster_2cell vector
@@ -78,12 +86,6 @@ cluster_2cell$cookiePalaeoLng <- NA
 cluster_2cell_raw <- cluster_2cell
 cluster_2cell_stdOccs_noRepl <- cluster_2cell
 cluster_2cell_stdOccs_Repl <- cluster_2cell
-
-## Output directory
-output.dir <- "~/Library/CloudStorage/Dropbox/unfinished_projects/bivalve_brachiopod/traybake_output"
-
-## Read in richness function
-source("functions/richness.R")
 
 ## Get stages from cluster_2cell
 stages <- as.character(sort(as.numeric(unique(cluster_2cell[,"stage"]))))
@@ -173,6 +175,74 @@ for(i in timeData$number){
 ## Export
 write.csv(cluster_2cell_stdOccs_noRepl, file = "data/analysis_data/genera_2cell_stdOccs_noRepl.csv")
 
+##### Genera - 2 cells, 50km cells, 100km radius, standardised occurrences with replacement #####
+## Set number of occurrences to be drawn as minimum
+nOccs <- 50
+
+## Update stages
+## For each stage, load data, summarise across clusters
+start = 1
+for(i in start:length(stages)){
+  ## try reading in data -
+  tray <- suppressWarnings(tryCatch(readRDS(paste0(output.dir, "/genera_stage", stages[i], "_", nCells, "GridCells.Rds")), error = function(e){}))
+  if(is.null(tray)){
+    next
+  } else {
+    ## get clusters
+    clusters <- 1:length(tray[[1]])
+    ## for each cluster
+    for(c in clusters){
+      ## Get row from cluster and stage
+      row <- which(cluster_2cell_stdOccs_Repl$cluster == str_flatten(c(stages[i],c),collapse = "_"))
+      ## Isolate clusters
+      cluster <- lapply(1:length(tray), function(x) tray[[x]][[c]])
+      ## Convert clusters into vectors of data
+      values <- mclapply(1:length(cluster), mc.cores = core.set, function(y){
+        ## isolate covariates, remove duplicates
+        covariates <- cluster[[y]][,c(40,41,42,45,46,47)]
+        covariates <- covariates[!duplicated(covariates),]
+        covs <- apply(covariates[,-1], 2, function(x) mean(x))
+        names(covs) <- NULL
+        ## richness
+        rich <- richness(data = cluster[[y]], cellID = "cell_50km", nCells, nDraws = nBites, nOccs = nOccs, standardiseOccs = T, standardiseOccsWithReplacement = T, taxonName = "combined_name", taxaToTally = c("Mollusca", "Brachiopoda"), n.cores = 1)
+        ## Combine output
+        out <- c(rich, covs[3], covs[4], covs[5], abs(covs[2]), covs[2], covs[1])
+      })
+      ## Get median values
+      medValues <- apply(t(sapply(1:length(values), function(x) values[[x]])), 2, function(z) median(z))
+      ## Populate
+      cluster_2cell_stdOccs_Repl[row, c(3:ncol(cluster_2cell_stdOccs_Repl))] <- medValues
+    }
+    ## Save to keep track in between stages
+    write.csv(cluster_2cell_stdOccs_Repl, file = "data/analysis_data/genera_2cell_stdOccs_Repl.csv")
+  }
+}
+
+
+## Add term for pre/post permian and stage midpoints
+timeData <- read.csv("data/metadata/binning_timescale.csv", row.names = 1)
+timeData$number <- seq(1,nrow(timeData),1)
+
+## Add containers
+cluster_2cell_stdOccs_Repl$stage_midpoint <- NA
+cluster_2cell_stdOccs_Repl$PTME <- NA
+
+## Stage 48 is last pre-PTME
+## for each timeData
+for(i in timeData$number){
+  if(any(cluster_2cell_stdOccs_Repl[,"stage"]==i)){
+    cluster_2cell_stdOccs_Repl[which(cluster_2cell_stdOccs_Repl$stage == i),"stage_midpoint"] <- timeData[i,"midpoint"]
+    if(i <= 48){
+      cluster_2cell_stdOccs_Repl[which(cluster_2cell_stdOccs_Repl$stage == i),"PTME"] <- "PrePTME"
+    } else {
+      cluster_2cell_stdOccs_Repl[which(cluster_2cell_stdOccs_Repl$stage == i),"PTME"] <- "PostPTME"
+    }
+  }
+}
+
+## Export
+write.csv(cluster_2cell_stdOccs_Repl, file = "data/analysis_data/genera_2cell_stdOccs_Repl.csv")
+
 ##### Genera - 2 cells, 50km cells, 100km radius, raw #####
 ## For each stage, load data, summarise across clusters
 start = 1
@@ -216,13 +286,10 @@ for(i in start:length(stages)){
 ## Read in genera count
 genera_count <- read.csv("data/sensitivity_testing/genera_50_radius_nSite_sensitivity_testing.csv", header = T, row.names = 1)
 
-## Isolate relevant counts
+## Update number of cells
 nCells <- 3
-radius <- 100000
 
-## Update number of batches to
-nBatch <- 500
-nBites <- 500
+## Isolate relevant data
 rel <- genera_count[intersect(which(genera_count$grid_cells == nCells), which(genera_count$radius == radius)),]
 
 ## Inititlise cluster_3cell vector
@@ -252,12 +319,6 @@ cluster_3cell$cookiePalaeoLng <- NA
 cluster_3cell_raw <- cluster_3cell
 cluster_3cell_stdOccs_noRepl <- cluster_3cell
 cluster_3cell_stdOccs_Repl <- cluster_3cell
-
-## Output directory
-output.dir <- "~/Library/CloudStorage/Dropbox/unfinished_projects/bivalve_brachiopod/traybake_output"
-
-## Read in richness function
-source("functions/richness.R")
 
 ## Get stages from cluster_3cell
 stages <- as.character(sort(as.numeric(unique(cluster_3cell[,"stage"]))))
@@ -347,6 +408,74 @@ for(i in timeData$number){
 ## Export
 write.csv(cluster_3cell_stdOccs_noRepl, file = "data/analysis_data/genera_3cell_stdOccs_noRepl.csv")
 
+##### Genera - 3 cells, 50km cells, 100km radius, standardised occurrences with replacement #####
+## Set number of occurrences to be drawn as minimum
+nOccs <- 50
+
+## Update stages
+## For each stage, load data, summarise across clusters
+start = 1
+for(i in start:length(stages)){
+  ## try reading in data -
+  tray <- suppressWarnings(tryCatch(readRDS(paste0(output.dir, "/genera_stage", stages[i], "_", nCells, "GridCells.Rds")), error = function(e){}))
+  if(is.null(tray)){
+    next
+  } else {
+    ## get clusters
+    clusters <- 1:length(tray[[1]])
+    ## for each cluster
+    for(c in clusters){
+      ## Get row from cluster and stage
+      row <- which(cluster_3cell_stdOccs_Repl$cluster == str_flatten(c(stages[i],c),collapse = "_"))
+      ## Isolate clusters
+      cluster <- lapply(1:length(tray), function(x) tray[[x]][[c]])
+      ## Convert clusters into vectors of data
+      values <- mclapply(1:length(cluster), mc.cores = core.set, function(y){
+        ## isolate covariates, remove duplicates
+        covariates <- cluster[[y]][,c(40,41,42,45,46,47)]
+        covariates <- covariates[!duplicated(covariates),]
+        covs <- apply(covariates[,-1], 2, function(x) mean(x))
+        names(covs) <- NULL
+        ## richness
+        rich <- richness(data = cluster[[y]], cellID = "cell_50km", nCells, nDraws = nBites, nOccs = nOccs, standardiseOccs = T, standardiseOccsWithReplacement = T, taxonName = "combined_name", taxaToTally = c("Mollusca", "Brachiopoda"), n.cores = 1)
+        ## Combine output
+        out <- c(rich, covs[3], covs[4], covs[5], abs(covs[2]), covs[2], covs[1])
+      })
+      ## Get median values
+      medValues <- apply(t(sapply(1:length(values), function(x) values[[x]])), 2, function(z) median(z))
+      ## Populate
+      cluster_3cell_stdOccs_Repl[row, c(3:ncol(cluster_3cell_stdOccs_Repl))] <- medValues
+    }
+    ## Save to keep track in between stages
+    write.csv(cluster_3cell_stdOccs_Repl, file = "data/analysis_data/genera_3cell_stdOccs_Repl.csv")
+  }
+}
+
+
+## Add term for pre/post permian and stage midpoints
+timeData <- read.csv("data/metadata/binning_timescale.csv", row.names = 1)
+timeData$number <- seq(1,nrow(timeData),1)
+
+## Add containers
+cluster_3cell_stdOccs_Repl$stage_midpoint <- NA
+cluster_3cell_stdOccs_Repl$PTME <- NA
+
+## Stage 48 is last pre-PTME
+## for each timeData
+for(i in timeData$number){
+  if(any(cluster_3cell_stdOccs_Repl[,"stage"]==i)){
+    cluster_3cell_stdOccs_Repl[which(cluster_3cell_stdOccs_Repl$stage == i),"stage_midpoint"] <- timeData[i,"midpoint"]
+    if(i <= 48){
+      cluster_3cell_stdOccs_Repl[which(cluster_3cell_stdOccs_Repl$stage == i),"PTME"] <- "PrePTME"
+    } else {
+      cluster_3cell_stdOccs_Repl[which(cluster_3cell_stdOccs_Repl$stage == i),"PTME"] <- "PostPTME"
+    }
+  }
+}
+
+## Export
+write.csv(cluster_3cell_stdOccs_Repl, file = "data/analysis_data/genera_3cell_stdOccs_Repl.csv")
+
 ##### Genera - 3 cells, 50km cells, 100km radius, raw #####
 ## For each stage, load data, summarise across clusters
 start = 1
@@ -390,13 +519,10 @@ for(i in start:length(stages)){
 ## Read in genera count
 genera_count <- read.csv("data/sensitivity_testing/genera_50_radius_nSite_sensitivity_testing.csv", header = T, row.names = 1)
 
-## Isolate relevant counts
+## Set number of cells
 nCells <- 4
-radius <- 100000
 
-## Update number of batches to
-nBatch <- 500
-nBites <- 500
+## Isolate relevant data
 rel <- genera_count[intersect(which(genera_count$grid_cells == nCells), which(genera_count$radius == radius)),]
 
 ## Inititlise cluster_4cell vector
@@ -423,15 +549,9 @@ cluster_4cell$cookiePalaeoLat <- NA
 cluster_4cell$cookiePalaeoLng <- NA
 
 ## Create three outputs
-cluster_3cell_raw <- cluster_3cell
-cluster_3cell_stdOccs_noRepl <- cluster_3cell
-cluster_3cell_stdOccs_Repl <- cluster_3cell
-
-## Output directory
-output.dir <- "~/Library/CloudStorage/Dropbox/unfinished_projects/bivalve_brachiopod/traybake_output"
-
-## Read in richness function
-source("functions/richness.R")
+cluster_4cell_raw <- cluster_4cell
+cluster_4cell_stdOccs_noRepl <- cluster_4cell
+cluster_4cell_stdOccs_Repl <- cluster_4cell
 
 ## Get stages from cluster_4cell
 stages <- as.character(sort(as.numeric(unique(cluster_4cell[,"stage"]))))
@@ -520,6 +640,74 @@ for(i in timeData$number){
 
 ## Export
 write.csv(cluster_4cell_stdOccs_noRepl, file = "data/analysis_data/genera_4cell_stdOccs_noRepl.csv")
+
+##### Genera - 4 cells, 50km cells, 100km radius, standardised occurrences with replacement #####
+## Set number of occurrences to be drawn as minimum
+nOccs <- 50
+
+## Update stages
+## For each stage, load data, summarise across clusters
+start = 1
+for(i in start:length(stages)){
+  ## try reading in data -
+  tray <- suppressWarnings(tryCatch(readRDS(paste0(output.dir, "/genera_stage", stages[i], "_", nCells, "GridCells.Rds")), error = function(e){}))
+  if(is.null(tray)){
+    next
+  } else {
+    ## get clusters
+    clusters <- 1:length(tray[[1]])
+    ## for each cluster
+    for(c in clusters){
+      ## Get row from cluster and stage
+      row <- which(cluster_4cell_stdOccs_Repl$cluster == str_flatten(c(stages[i],c),collapse = "_"))
+      ## Isolate clusters
+      cluster <- lapply(1:length(tray), function(x) tray[[x]][[c]])
+      ## Convert clusters into vectors of data
+      values <- mclapply(1:length(cluster), mc.cores = core.set, function(y){
+        ## isolate covariates, remove duplicates
+        covariates <- cluster[[y]][,c(40,41,42,45,46,47)]
+        covariates <- covariates[!duplicated(covariates),]
+        covs <- apply(covariates[,-1], 2, function(x) mean(x))
+        names(covs) <- NULL
+        ## richness
+        rich <- richness(data = cluster[[y]], cellID = "cell_50km", nCells, nDraws = nBites, nOccs = nOccs, standardiseOccs = T, standardiseOccsWithReplacement = F, taxonName = "combined_name", taxaToTally = c("Mollusca", "Brachiopoda"), n.cores = 1)
+        ## Combine output
+        out <- c(rich, covs[3], covs[4], covs[5], abs(covs[2]), covs[2], covs[1])
+      })
+      ## Get median values
+      medValues <- apply(t(sapply(1:length(values), function(x) values[[x]])), 2, function(z) median(z))
+      ## Populate
+      cluster_4cell_stdOccs_Repl[row, c(3:ncol(cluster_4cell_stdOccs_Repl))] <- medValues
+    }
+    ## Save to keep track in between stages
+    write.csv(cluster_4cell_stdOccs_Repl, file = "data/analysis_data/genera_4cell_stdOccs_Repl.csv")
+  }
+}
+
+
+## Add term for pre/post permian and stage midpoints
+timeData <- read.csv("data/metadata/binning_timescale.csv", row.names = 1)
+timeData$number <- seq(1,nrow(timeData),1)
+
+## Add containers
+cluster_4cell_stdOccs_Repl$stage_midpoint <- NA
+cluster_4cell_stdOccs_Repl$PTME <- NA
+
+## Stage 48 is last pre-PTME
+## for each timeData
+for(i in timeData$number){
+  if(any(cluster_4cell_stdOccs_Repl[,"stage"]==i)){
+    cluster_4cell_stdOccs_Repl[which(cluster_4cell_stdOccs_Repl$stage == i),"stage_midpoint"] <- timeData[i,"midpoint"]
+    if(i <= 48){
+      cluster_4cell_stdOccs_Repl[which(cluster_4cell_stdOccs_Repl$stage == i),"PTME"] <- "PrePTME"
+    } else {
+      cluster_4cell_stdOccs_Repl[which(cluster_4cell_stdOccs_Repl$stage == i),"PTME"] <- "PostPTME"
+    }
+  }
+}
+
+## Export
+write.csv(cluster_4cell_stdOccs_Repl, file = "data/analysis_data/genera_4cell_stdOccs_Repl.csv")
 
 ##### Genera - 4 cells, 50km cells, 100km radius, raw #####
 ## For each stage, load data, summarise across clusters
